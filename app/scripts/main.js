e0df0591687577e599bf75f02e156eedf96cb725
Michael Roussetos <mihalirou@gmail.com>
2014-10-02 19:42:14 -0400

    Done

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -9,6 +9,13 @@
       $('#nav-toggle').toggleClass('active');
     });
 
+    $('#header').stop().delay(2300).animate({'opacity': '0'}, 1500);
+    $('#header').hover(
+    function() {$('#header').stop().animate({'opacity': '1'}, 1000);},
+    function() {$('#header').stop().animate({'opacity': '0'}, 1500);}
+    );
+
+
   });
 })(jQuery);
 
